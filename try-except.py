51fd7f2e48905f5345365faa5e5fe8e32f2be5f6
Sergio Semprebom <sergiosemprebom@gmail.com>
2024-07-27 03:57:50 -0300

    exercicios try-except

--- try-except.py
+++ try-except.py
@@ -3,6 +3,7 @@ Escreva um programa que converta a temperatura de Celsius para Fahrenheit.
 O programa deve solicitar ao usuário a temperatura em Celsius e, utilizando try-except, 
 garantir que a entrada seja numérica, tratando qualquer ValueError. Imprima o resultado 
 em Fahrenheit ou uma mensagem de erro se a entrada não for válida.'''
+print('Exercicio 21')
 try:
     celsius = int(input("Digite um valor em Celsius: "))
     fahrenheit = (celsius * 9/5) + 32
@@ -16,7 +17,7 @@ print('=========================================================================
 Crie um programa que verifica se uma palavra ou frase é um palíndromo (lê-se igualmente 
 de trás para frente, desconsiderando espaços e pontuações). Utilize try-except para garantir 
 que a entrada seja uma string. Dica: Utilize a função isinstance() para verificar o tipo da entrada.'''
-
+print('Exercicio 22')
 entrada = input("Digite uma palavra: ")
 if isinstance(entrada, str):
     formatado = entrada.replace(" ","").lower()
@@ -36,11 +37,57 @@ por zero e entradas não numéricas. Utilize if-elif-else para realizar a
 operação matemática baseada no operador fornecido. Imprima o resultado 
 ou uma mensagem de erro apropriada.'''
 
+print('Exercício 23: Calculadora Simples')
+try:
+    n1 = int(input("Digite o primeiro numero: "))
+    n2 = int(input("Digite o segundo numero: "))
+
+    print(f"{n1} X {n2} = {n1*n2}")
+    
+except ValueError:
+    print("Erro: Entrada inválido ou divisão por zero")
+print('==========================================================================================')
+
+
+cm = int(input("Digite um numero: "))
+i = 1
+try:
+    while i <= 10:
+        print(f'{cm} X {i} = {cm * i}')
+        i += 1
+except ValueError:
+    print("Erro: Por favor digite um numero valido")
+print('==========================================================================================')
+
 '''Exercício 24: Classificador de Números
 Escreva um programa que solicite ao usuário para digitar um número. 
 Utilize try-except para assegurar que a entrada seja numérica e utilize 
 if-elif-else para classificar o número como "positivo", "negativo" 
 ou "zero". Adicionalmente, identifique se o número é "par" ou "ímpar".'''
+
+print("Exercício 24: Classificador de Números")
+try:
+    num24 = int(input("Digite um numero: "))
+ 
+    if num24 > 0: 
+        print("O numero é positivo:")
+        
+    elif num24 < 0:
+        print("O numero é negativo")
+
+    else:
+        print("O numero é zero")
+        
+    if num24 % 2 == 0:
+        print("Numero PAR")
+        
+    else:
+        print("Numero ÍMPAR")
+    
+except ValueError:
+    print("Erro: Por favor digite um numero para ser valido.")
+print('==========================================================================================')
+
 
 
 '''Exercício 25: Conversão de Tipo com Validação
